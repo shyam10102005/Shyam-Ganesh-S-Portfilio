125623a5ab4a01db7514c4b1e5d8f323d42ad1de
Shyam Ganesh S <shyam10102005@gmail.com>
2026-01-18 15:45:06 +0530

    Setup GitHub Pages deployment with HashRouter

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url"
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    base: "/Shyam-Ganesh-S-Portfilio/",
     plugins: [react()],
     resolve: {
         alias: {
